bb1c90a06c42bcc58f71d50d23f7d9ccfbf438d3
pizzamammapizza <ilia.nekto@gmail.com>
2023-08-21 13:59:31 +0200

    easy

--- src/main/java/org/botcontrol/Bot.java
+++ src/main/java/org/botcontrol/Bot.java
@@ -9,14 +9,13 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Bot extends TelegramLongPollingBot{
     private final ReplyKeyboardMarkup rkm;
     private final MessageSender messageSender;
     private final BotAnswers botAnswers;
     private final long[] idList = new long[]{1547968115, 586833144};
+    private static final int CONNUM_LENGTH = 19;
 //    private final long[] idList = new long[]{1547968115};
     public Bot(){
         KeyboardRow kr1 = new KeyboardRow();
@@ -58,12 +57,10 @@ public class Bot extends TelegramLongPollingBot{
             }
         }
     }
-    private boolean isCorrectConnum(String text){
+    private boolean isCorrectConnum(String text) {
         if (!text.startsWith("/connum")) return false;
-        if (text.length()!=19) return false;
-        Pattern regexPattern = Pattern.compile("^\\d+$"); // Паттерн который проверяет, все ли символы в стринге - числа. Если нет, то хуйня полная(( плакать можно((
-        Matcher matcher = regexPattern.matcher(text.substring(8)); //Собсна сама проврека, которая в строке ниже вернет тру или фолс
-        return matcher.matches();
+        if (text.length() != CONNUM_LENGTH) return false;
+        return text.substring(8).matches("^\\d+$");
     }
     private boolean isUsernameVisible(Update update){
         if (update.getMessage().getChat().getUserName() == null){
@@ -76,9 +73,6 @@ public class Bot extends TelegramLongPollingBot{
             return true;
         }
     }
-    /*
-    НАДО ИЗМЕНИТЬ КАК-ТО ЧТОБЫ ПОСЛЕ ОПЛАТЫ БЫЛО ЧТО ЛИ
-     */
     private boolean isOneOfSubs(String msg){
         switch(msg){
             case "3 months", "12 months", "6 months" -> {
